a902d1301cd947f1f53b868e0b654414a24b8b80
Juan Carlos Diaz <jcdiaz.vale@gmail.com>
2018-09-27 21:00:54 -0400

    seguimos actualizando

--- assets/js/funciones_formularios.js
+++ assets/js/funciones_formularios.js
@@ -1,7 +1,6 @@
-var suma=function(numero1, numero2){
-    var numero1 = parseInt(document.getElementById("numero1").Value);
-    var numero2 = parseInt(document.getElementById("numero2").Value);
-    
+var suma = function(numero1, numero2){
+    var numero1 = parseInt(numero1.value);
+    var numero2 = parseInt(numero2.value);
     var resultado = numero1+numero2;
     return resultado;
-}
+    }
